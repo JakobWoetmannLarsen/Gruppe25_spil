91433f365b691da580c863783955daf9cfbd1a8b
JakobWoetmannLarsen <44495454+JakobWoetmannLarsen@users.noreply.github.com>
2018-11-15 10:01:33 +0100

    Copy struktur fra tidligere spil

---
+++ script.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("siden vises");
+
+    showStart();
+
+}
+
+
+function showStart() {}
+
+
+function showMenu() {}
+
+
+function hideMenu() {}
+
+
+function hideStart() {}
+
+
+function showIntro() {}
+
+
+function hideIntro() {}
+
+
+function startGame() {}
